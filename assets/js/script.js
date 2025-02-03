5f5abce4502143f7a8297fe8ec52138a731c2d87
Malvin S. Eikemo <malvin.se.ei@gmail.com>
2025-02-03 12:51:28 +0000

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+    var navbar = document.getElementById("navbar")
+    var sticky = navbar.offsetTop
+  
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky")
+    }
+  })
+  
+  
